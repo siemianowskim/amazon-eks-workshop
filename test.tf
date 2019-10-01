b53a1e24397850d976d54f89f60fbdaa8e98f8f0
siemianowskim <siemianowski.michal@gmail.com>
2019-10-01 16:21:00 +0100

    test

--- test.tf
+++ test.tf
@@ -1,6 +1,13 @@
+variable "region" {
+  default = "eu-west-1"
+}
+
+provider "aws" {
+  region     = var.region
+}
+
 resource "aws_efs_file_system" "foo" {
   creation_token = "my-product"
-  region = "eu-west-1"
   tags = {
     Name = "MyProduct"
   }
